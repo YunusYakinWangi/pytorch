a12f61cc84ee57550bf0628821916290a223422c
Xuehai Pan <XuehaiPan@pku.edu.cn>
2026-03-21 16:33:13 +0800

    Update
    [ghstack-poisoned]

--- torch/utils/pytree/__init__.py
+++ torch/utils/pytree/__init__.py
@@ -17,7 +17,7 @@ collection support for PyTorch APIs.
 import os as _os
 import sys as _sys
 from types import ModuleType as _ModuleType
-from typing import Any as _Any, TYPE_CHECKING as _TYPE_CHECKING
+from typing import Any as _Any, TYPE_CHECKING as _TYPE_CHECKING, TypeAlias as _TypeAlias
 
 import torch.utils._pytree as python
 from torch.utils._pytree import (  # these type aliases are identical in both implementations
@@ -198,6 +198,9 @@ else:
         tree_unflatten,
         treespec_pprint,
     )
+
+
+TreeSpec: _TypeAlias = PyTreeSpec
 
 
 def register_pytree_node(
