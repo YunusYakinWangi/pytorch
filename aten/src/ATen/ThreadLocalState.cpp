8ff1057310c00579ebfca8387eaf878b428bd1b0
Rob Timpe <rtimpe@openteams.com>
2026-04-17 18:25:04 +0000

    Fix deadlock related to _stash_obj_in_tls
    There's potential deadlock related to _stash_obj_in_tls.  As of https://github.com/pytorch/pytorch/pull/173568 the
    context object is stored in the calling thread TLS during _engine_run_backward.  Normally this object deallocated after
    the backward pass completes, but there's a race where the autograd device thread can end up holding the last reference.
    If this happens the context object gets deallocated by the device thread, which needs to acquire the GIL.  If another
    thread happens to be holding the GIL while blocked on the device thread the result is a deadlock.  See
    https://github.com/pytorch/pytorch/pull/173568 for more detail
    
    The fix here avoids aquiring the GIL in the device thread by clearing the GraphTask's shared_ptr to the context object
    during cleanup.  Since the TLS in the calling thread also has a shared_ptr, this avoids calling the SafePyObject
    destructor (and therefore acquiring the GIL).  I'm not a huge fan of this approach since it introduces a manual cleanup
    step, but I wasn't able to come up with something better.  Another option would be to use SafePyHandle instead of
    SafePyObject and introduce a `_remove_obj_from_tls` python API.  But that makes the python side responsible for
    refcounting, which feels wrong.
    
    I'm also not sure exactly how we end up with a thread holding the GIL and also blocked on an autograd device thread.
    That feels like a bug, but it seems to be triggered by Meta-internal code, so I'm not able to debug it.  All of which is
    to say that this is the best approach I can come up with, but I'm very open to other suggestions.
    
    
    ghstack-source-id: 81c970362d1af99f408563037c006a7e995f50cc
    Pull-Request: https://github.com/pytorch/pytorch/pull/180700

--- aten/src/ATen/ThreadLocalState.cpp
+++ aten/src/ATen/ThreadLocalState.cpp
@@ -36,6 +36,10 @@ void ThreadLocalState::set_multithreading_enabled(bool enabled) {
   autograd_tls_.set_multithreading_enabled(enabled);
 }
 
+void ThreadLocalState::clear_saved_py_objects() {
+  saved_objects_.clear_objs();
+}
+
 /* static */
 void ThreadLocalState::setThreadLocalState(
     const ThreadLocalState& state) {
